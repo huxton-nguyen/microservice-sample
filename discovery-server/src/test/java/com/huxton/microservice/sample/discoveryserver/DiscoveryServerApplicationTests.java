23f6138f68a16589a45f39cca82e7c85f9d0c7db
hoang <hoangnv111220@gmail.com>
2024-03-12 13:56:44 +0700

    configure discovery sever and registry services

---
+++ discovery-server/src/test/java/com/huxton/microservice/sample/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huxton.microservice.sample.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
